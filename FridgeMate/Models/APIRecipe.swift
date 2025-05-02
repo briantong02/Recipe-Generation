a26114b11e63b34f47ba6c08a072aa29f68d7a7f
Chaeyeon Yu <chaeyyn11@gmail.com>
2025-05-02 17:05:14 +1000

    refactor(ingredient): introduce APIIngredient and init(api:) converter

---
+++ FridgeMate/Models/APIRecipe.swift
@@ -0,0 +1,18 @@
+//
+//  APIRecipe.swift
+//  FridgeMate
+//
+//  Created by Chaeyeon Yu on 2/5/2025.
+//
+
+import SwiftUI
+
+struct APIRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APIRecipe()
+}
