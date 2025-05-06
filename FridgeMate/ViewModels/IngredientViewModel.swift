2ed4f5b669aa46893351725d2f6f412ddbc17f63
Rujit <rujeetpraz@gmail.com>
2025-05-06 11:54:50 +1000

    delete redundant code files

--- FridgeMate/ViewModels/IngredientViewModel.swift
+++ FridgeMate/ViewModels/IngredientViewModel.swift
@@ -11,38 +11,5 @@ class IngredientViewModel: ObservableObject{
     
     @Published var ingredients: [Ingredient] = []
     
-    let session  = URLSession(configuration: .default)
-    
-    func searchIngredient(query: String, completion: @escaping ([Ingredient]?) -> Void){
-        let urlString = "\(Constant().baseURL)/food/ingredients/search?query=\(query)"
-        
-        guard let url = URL(string: urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!) else {
-            completion(nil)
-            return
-        }
-        
-        
-        URLSession.shared.dataTask(with: url){
-            data,response,error in
-            if let error = error{
-                
-            }
-            if let data = data {
-                do {
-                    let decoded = try JSONDecoder().decode(IngredientSearchResult.self, from: data)
-                    DispatchQueue.main.async {
-                        completion(decoded.results)
-                    }
-                } catch {
-                    print("Decoding error:", error)
-                    completion(nil)
-                }
-            } else {
-                print("Network error:", error ?? "Unknown error")
-                completion(nil)
-            }
-        }
-        .resume()
-    }
     
 }
