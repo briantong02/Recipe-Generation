ad3dccf23aea96b75aacc8e5e4f73d4944600430
Chaeyeon Yu <chaeyyn11@gmail.com>
2025-05-09 14:12:29 +1000

    Merge all the files

---
+++ FridgeMate/Views/Saved Recipe/SavedRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedRecipeView.swift
+//  FridgeMate
+//
+//  Created by Chaeyeon Yu on 9/5/2025.
+//
+
+import SwiftUI
+
+struct SavedRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedRecipeView()
+}
